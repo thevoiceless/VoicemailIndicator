4bc08745d23ff750a7b48fb0910a2ca77fd22205
Riley Moses <victi.resurgo@gmail.com>
2014-11-26 13:03:59 -0700

    Add button to trigger test notifications

--- app/src/main/java/thevoiceless/voicemailindicator/activities/MainActivity.java
+++ app/src/main/java/thevoiceless/voicemailindicator/activities/MainActivity.java
@@ -1,19 +1,31 @@
 package thevoiceless.voicemailindicator.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.List;
-
 import thevoiceless.voicemailindicator.R;
+import thevoiceless.voicemailindicator.services.VoicemailDetectorService;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String NOTIFICATION_TAG = "voicemail_indicator_notification";
+
+    private static int sNotificationId = 1;
+
+    private Button mTriggerNotificationButton;
     private TextView mEmptyText;
+    // TODO: RecyclerView
     private ListView mList;
 
     @Override
@@ -21,8 +33,35 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTriggerNotificationButton = ((Button) findViewById(R.id.trigger_notification));
         mEmptyText = ((TextView) findViewById(R.id.empty));
         mList = ((ListView) findViewById(R.id.list));
+
+        init();
+
+        startService(new Intent(this, VoicemailDetectorService.class));
+    }
+
+    private void init() {
+        mTriggerNotificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Creating test notification");
+                NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                if (nm != null) {
+                    Log.i(TAG, "Created notification manager");
+                    Notification notification = new NotificationCompat.Builder(MainActivity.this)
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .setContentTitle("Test notification")
+                            .setContentText("This is a test")
+                            .build();
+                    if (notification != null) {
+                        Log.i(TAG, "Created notification, calling notify()");
+                        nm.notify(NOTIFICATION_TAG, sNotificationId++, notification);
+                    }
+                }
+            }
+        });
         mList.setEmptyView(mEmptyText);
     }
 
--- app/src/main/java/thevoiceless/voicemailindicator/services/VoicemailDetectorService.java
+++ app/src/main/java/thevoiceless/voicemailindicator/services/VoicemailDetectorService.java
@@ -1,23 +1,39 @@
 package thevoiceless.voicemailindicator.services;
 
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 public class VoicemailDetectorService extends NotificationListenerService {
 
+    private static final String TAG = VoicemailDetectorService.class.getSimpleName();
+
     public VoicemailDetectorService() {
+        Log.i(TAG, "Constructor");
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+        Log.i(TAG, "onNotificationPosted");
         super.onNotificationPosted(sbn);
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG, "onNotificationRemoved");
         super.onNotificationRemoved(sbn);
     }
 }
