b171d8ed40caf88083f30c2caefe65408fe6613e
Riley Moses <victi.resurgo@gmail.com>
2014-11-30 14:56:12 -0500

    Add phone state listener

--- app/src/main/java/thevoiceless/voicemailindicator/activities/MainActivity.java
+++ app/src/main/java/thevoiceless/voicemailindicator/activities/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,66 +22,142 @@ public class MainActivity extends ActionBarActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String NOTIFICATION_TAG = "voicemail_indicator_notification";
+    private static final String KEY_SERVICE = "key_service";
+    private static final String KEY_LISTENER = "key_listener";
 
     private static int sNotificationId = 1;
 
+    private Button mStartServiceButton;
+    private Button mStopServiceButton;
+    private Button mAddPhoneStateListenerButton;
+    private Button mRemovePhoneStateListenerButton;
     private Button mTriggerNotificationButton;
     private TextView mEmptyText;
     // TODO: RecyclerView
     private ListView mList;
+
+    private boolean mServiceRunning;
+    private boolean mAddedListener;
+
+    private PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
+        @Override
+        public void onMessageWaitingIndicatorChanged(boolean mwi) {
+            Log.i(TAG, String.format("'Message waiting' indicator changed: %s", String.valueOf(mwi)));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTriggerNotificationButton = ((Button) findViewById(R.id.trigger_notification));
-        mEmptyText = ((TextView) findViewById(R.id.empty));
-        mList = ((ListView) findViewById(R.id.list));
+        if (savedInstanceState != null) {
+            mServiceRunning = savedInstanceState.getBoolean(KEY_SERVICE);
+            mAddedListener = savedInstanceState.getBoolean(KEY_LISTENER);
+        }
 
         init();
+    }
 
-        startService(new Intent(this, VoicemailDetectorService.class));
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(KEY_SERVICE, mServiceRunning);
+        outState.putBoolean(KEY_LISTENER, mAddedListener);
     }
 
     private void init() {
-        mTriggerNotificationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Creating test notification");
-                NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                if (nm != null) {
-                    Log.i(TAG, "Created notification manager");
-                    Notification notification = new NotificationCompat.Builder(MainActivity.this)
-                            .setSmallIcon(R.drawable.ic_launcher)
-                            .setContentTitle("Test notification")
-                            .setContentText("This is a test")
-                            .build();
-                    if (notification != null) {
-                        Log.i(TAG, "Created notification, calling notify()");
-                        nm.notify(NOTIFICATION_TAG, sNotificationId++, notification);
-                    }
-                }
-            }
-        });
+        mStartServiceButton = ((Button) findViewById(R.id.start_service));
+        mStopServiceButton = ((Button) findViewById(R.id.stop_service));
+        mAddPhoneStateListenerButton = ((Button) findViewById(R.id.add_state_listener));
+        mRemovePhoneStateListenerButton = ((Button) findViewById(R.id.remove_state_listener));
+        mTriggerNotificationButton = ((Button) findViewById(R.id.trigger_notification));
+
+        mEmptyText = ((TextView) findViewById(R.id.empty));
+        mList = ((ListView) findViewById(R.id.list));
+
         mList.setEmptyView(mEmptyText);
+
+        updateButtons();
     }
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void updateButtons() {
+        if (mServiceRunning) {
+            mStartServiceButton.setEnabled(false);
+            mAddPhoneStateListenerButton.setEnabled(false);
+            mRemovePhoneStateListenerButton.setEnabled(false);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+            mStopServiceButton.setEnabled(true);
+        } else {
+            mStartServiceButton.setEnabled(true);
+            mAddPhoneStateListenerButton.setEnabled(true);
+            mRemovePhoneStateListenerButton.setEnabled(true);
+
+            mStopServiceButton.setEnabled(false);
+        }
+
+        if (mAddedListener) {
+            mAddPhoneStateListenerButton.setEnabled(false);
+            mStartServiceButton.setEnabled(false);
+            mStopServiceButton.setEnabled(false);
+
+            mRemovePhoneStateListenerButton.setEnabled(true);
+        } else {
+            mAddPhoneStateListenerButton.setEnabled(true);
+            mStartServiceButton.setEnabled(true);
+            mStopServiceButton.setEnabled(true);
+
+            mRemovePhoneStateListenerButton.setEnabled(false);
         }
     }
-    */
+
+    public void onStartServiceClicked(View v) {
+        Log.i(TAG, "Starting service");
+        startService(new Intent(this, VoicemailDetectorService.class));
+        mServiceRunning = true;
+        updateButtons();
+    }
+
+    public void onStopServiceClicked(View v) {
+        Log.i(TAG, "Stopping service");
+        stopService(new Intent(this, VoicemailDetectorService.class));
+        mServiceRunning = false;
+        updateButtons();
+    }
+
+    public void onAddListenerClicked(View v) {
+        Log.i(TAG, "Adding phone state listener");
+        TelephonyManager tm = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE));
+        if (tm != null) {
+            tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_MESSAGE_WAITING_INDICATOR);
+            mAddedListener = true;
+            updateButtons();
+        }
+    }
+
+    public void onRemoveListenerClicked(View v) {
+        Log.i(TAG, "Removing phone state listener");
+        TelephonyManager tm = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE));
+        if (tm != null) {
+            tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+            mAddedListener = false;
+            updateButtons();
+        }
+    }
+
+    public void onTriggerNotificationClicked(View v) {
+        Log.i(TAG, "Creating test notification");
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (nm != null) {
+            Log.i(TAG, "Created notification manager");
+            Notification notification = new NotificationCompat.Builder(MainActivity.this)
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle("Test notification")
+                    .setContentText("This is a test")
+                    .build();
+            if (notification != null) {
+                Log.i(TAG, "Created notification, calling notify()");
+                nm.notify(NOTIFICATION_TAG, sNotificationId++, notification);
+            }
+        }
+    }
 }
